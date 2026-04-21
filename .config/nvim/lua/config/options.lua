2ce3d5a55e9376b7ad53473357494d485619fc19
bart_dude <kanye@seek.li>
2026-04-21 23:53:52 +0800

    Changes to be committed: 	modified:   .config/nvim/lua/config/options.lua 	modified:   .config/tmux/tmux.conf

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,4 +2,17 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.clipboard = "unnamedplus"
+-- Yank to system clipboard, used very often.
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], {
+  desc = "Yank to system clipboard",
+})
+
+-- Paste without overwriting yank register
+vim.keymap.set("x", "<leader>p", [["_dP]], {
+  desc = "Paste without clobbering clipboard",
+})
+
+-- Delete without yanking
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], {
+  desc = "Delete without yank",
+})

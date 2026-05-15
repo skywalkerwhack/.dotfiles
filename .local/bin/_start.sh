cf16c9e0f9ef0624a960886e77d7fef0622fb115
bart_dude <kanye@seek.li>
2026-05-15 16:45:35 +0800

    modified:   .local/bin/_start.sh

--- .local/bin/_start.sh
+++ .local/bin/_start.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-cd "$HOME/sync/" && ./v2rayN-linux-64/v2rayN &
+cd "$HOME" && ./v2rayN-linux-64/v2rayN &
 sleep 10
 syncthing >/dev/null &
 # kitty --hold sh -c "$HOME/coding/sh/backup.sh"
